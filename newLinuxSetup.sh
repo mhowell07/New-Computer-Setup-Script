1b79251ce6e82bfaddcd40d01dbfe6ab3f0b823d
Matt Howell <matthew.howell07@gmail.com>
2018-09-20 21:34:44 -0500

    Added command to install PHP7 and related packages

--- newLinuxSetup.sh
+++ newLinuxSetup.sh
@@ -11,3 +11,6 @@ sudo apt-get install -y npm
 
 # Install NGINX
 sudo apt-get install -y nginx
+
+# Install PHP7 and related packages
+sudo apt-get install -y php7.0 php7.0-fpm php7.0-cli php7.0-common php7.0-mbstring php7.0-gd php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt php7.0-zip
